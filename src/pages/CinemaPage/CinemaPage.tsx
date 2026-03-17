59888c86a2beb765bc2eda82560376e28353d697
Andriy Sak <andreyysak17@gmail.com>
2026-03-17 16:39:08 +0200

    refactor: move topbar to movie page

--- src/pages/CinemaPage/CinemaPage.tsx
+++ src/pages/CinemaPage/CinemaPage.tsx
@@ -4,11 +4,8 @@ import Loader from '@/widgets/Loader'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { toast } from 'react-toastify'
 import { Autoplay } from 'swiper/modules'
-import { Clock, Eye, Heart, Info, Plus, Search, X } from 'lucide-react'
+import { Info, Plus } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
-import { Link } from 'react-router'
-import { useDebounce } from '@/shared/hooks/useDebounce.ts'
-import { useMoviesStore } from '@/entities/movies/model/movieStore.ts'
 
 const getRatingClass = (vote: number) => {
   if (vote >= 7) return 'high'
@@ -18,18 +15,10 @@ const getRatingClass = (vote: number) => {
 
 export const CinemaPage = () => {
   const { t } = useTranslation()
-  const {searchQuery, setSearchQuery} = useMoviesStore()
-
-  const debounceSearch = useDebounce(searchQuery, 500)
-
   const {
     moviesTrending,
     toggleWatchLater,
-    favoritesCount,
-    watchedCount,
-    watchLaterCount,
-    moviesByTitle,
-  } = useMovies(undefined, debounceSearch)
+  } = useMovies()
 
   useEffect(() => {
     if (moviesTrending.isError) {
@@ -47,101 +36,6 @@ export const CinemaPage = () => {
 
   return (
     <div className="cinema">
-      <div className="cinema__topbar">
-        <div className="cinema__topbar-input">
-          <Search />
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={(event) => setSearchQuery(event.target.value)}
-            placeholder={t('cinema.search_placeholder')}
-          />
-
-          <button
-            onClick={() => setSearchQuery('')}
-            className={searchQuery.length > 0 ? 'visible' : ''}
-          >
-            <X />
-          </button>
-        </div>
-
-        <nav className="cinema__topbar_navbar">
-          <ul>
-            <li>
-              <Link to="/favorites" className="cinema__topbar-link">
-                <Heart />
-                {favoritesCount > 0 && (
-                  <span className="cinema__topbar-badge">{favoritesCount}</span>
-                )}
-              </Link>
-            </li>
-            <li>
-              <Link to="/watched" className="cinema__topbar-link">
-                <Eye />
-                {watchedCount > 0 && (
-                  <span className="cinema__topbar-badge">{watchedCount}</span>
-                )}
-              </Link>
-            </li>
-            <li>
-              <Link to="/watch-later" className="cinema__topbar-link">
-                <Clock />
-                {watchLaterCount > 0 && (
-                  <span className="cinema__topbar-badge">{watchLaterCount}</span>
-                )}
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      {searchQuery && (
-        <div className="cinema__results">
-          {moviesByTitle.isFetching ? (
-            <div className="cinema__results-loader">
-              <div className="cinema__results-spinner"></div>
-            </div>
-          ) : (
-            <>
-              {moviesByTitle.data && moviesByTitle.data.length > 0 ? (
-                <>
-                  {moviesByTitle.data.slice(0, 5).map((movie) => (
-                    <Link
-                      to={`/movie/${movie.id}`}
-                      key={movie.id}
-                      className="cinema__results-row"
-                      onClick={() => setSearchQuery('')}
-                    >
-                      <img
-                        src={
-                          movie.poster_path
-                            ? `https://image.tmdb.org/t/p/w92/${movie.poster_path}`
-                            : '/placeholder.png'
-                        }
-                        alt={movie.title}
-                      />
-                      <div className="cinema__results-info">
-                        <h3 className="cinema__results-title">{movie.title}</h3>
-                        <p
-                          className={`cinema__results-vote cinema__results-vote--${getRatingClass(movie.vote_average)}`}
-                        >
-                          {movie.vote_average.toFixed(1)}
-                        </p>
-                      </div>
-                    </Link>
-                  ))}
-                  <div className="cinema__results-link">
-                    <Link to="/search-results">{t('cinema.search_result')}</Link>
-                  </div>
-                </>
-              ) : (
-                <div className="cinema__results-empty">{t('cinema.no_results')}</div>
-              )}
-            </>
-          )}
-        </div>
-      )}
-
       <div className="cinema__hero">
         <Swiper
           modules={[Autoplay]}
--- src/pages/MoviePage/MoviePage.tsx
+++ src/pages/MoviePage/MoviePage.tsx
@@ -1,5 +1,118 @@
+import { Link } from 'react-router-dom' // Додано імпорт
+import { Clock, Eye, Heart, Search, X } from 'lucide-react'
+import { useTranslation } from 'react-i18next'
+import { useMoviesStore } from '@/entities/movies/model/movieStore.ts'
+import { useDebounce } from '@/shared/hooks/useDebounce.ts'
+import { useMovies } from '@/entities/movies/api/movieQuery.ts'
+
+const getRatingClass = (vote: number) => {
+  if (vote >= 7) return 'high'
+  if (vote >= 5) return 'medium'
+  return 'low'
+}
+
 export const MoviePage = () => {
+  const { t } = useTranslation()
+  const { searchQuery, setSearchQuery } = useMoviesStore()
+  const debounceSearch = useDebounce(searchQuery, 500)
+
+  const { favoritesCount, watchedCount, watchLaterCount, moviesByTitle } = useMovies(
+    undefined,
+    debounceSearch,
+  )
+
   return (
-    <div>Movie</div>
+    <div className="movie">
+      <div className="movies__topbar">
+        <div className="movies__topbar-input">
+          <Search />
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(event) => setSearchQuery(event.target.value)}
+            placeholder={t('cinema.search_placeholder')}
+          />
+          <button
+            onClick={() => setSearchQuery('')}
+            className={searchQuery.length > 0 ? 'visible' : ''}
+          >
+            <X />
+          </button>
+        </div>
+
+        <nav className="movies__topbar_navbar">
+          <ul>
+            <li>
+              <Link to="/favorites" className="movies__topbar-link">
+                <Heart />
+                {favoritesCount > 0 && (
+                  <span className="movies__topbar-badge">{favoritesCount}</span>
+                )}
+              </Link>
+            </li>
+            <li>
+              <Link to="/watched" className="movies__topbar-link">
+                <Eye />
+                {watchedCount > 0 && (
+                  <span className="movies__topbar-badge">{watchedCount}</span>
+                )}
+              </Link>
+            </li>
+            <li>
+              <Link to="/watch-later" className="movies__topbar-link">
+                <Clock />
+                {watchLaterCount > 0 && (
+                  <span className="movies__topbar-badge">{watchLaterCount}</span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Результати пошуку тепер всередині топбару або відразу під ним */}
+        {searchQuery && (
+          <div className="movies__results">
+            {moviesByTitle.isFetching ? (
+              <div className="movies__results-loader">
+                <div className="movies__results-spinner"></div>
+              </div>
+            ) : moviesByTitle.data && moviesByTitle.data.length > 0 ? (
+              <>
+                {moviesByTitle.data.slice(0, 5).map((movie) => (
+                  <Link
+                    to={`/movie/${movie.id}`}
+                    key={movie.id}
+                    className="movies__results-row"
+                    onClick={() => setSearchQuery('')}
+                  >
+                    <img
+                      src={
+                        movie.poster_path
+                          ? `https://image.tmdb.org/t/p/w92/${movie.poster_path}`
+                          : '/placeholder.png'
+                      }
+                      alt={movie.title}
+                    />
+                    <div className="movies__results-info">
+                      <h3 className="movies__results-title">{movie.title}</h3>
+                      <p
+                        className={`movies__results-vote movies__results-vote--${getRatingClass(movie.vote_average)}`}
+                      >
+                        {movie.vote_average.toFixed(1)}
+                      </p>
+                    </div>
+                  </Link>
+                ))}
+                <div className="movies__results-link">
+                  <Link to="/search-results">{t('cinema.search_result')}</Link>
+                </div>
+              </>
+            ) : (
+              <div className="movies__results-empty">{t('cinema.no_results')}</div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
